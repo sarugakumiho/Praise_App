2f57d619a614d4ac53acc0fb4149201038d9632d
sarugakumiho <171490497+sarugakumiho@users.noreply.github.com>
2024-09-20 19:16:05 +0000

    [Update]バリデーションメッセージの調整

--- app/models/member.rb
+++ app/models/member.rb
@@ -45,9 +45,10 @@ class Member < ApplicationRecord
   # バリデーション
   validates :name, length: { minimum: 1, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 150 }
-  # メールの正規表現化
-  validates :email, presence: true, length: { maximum: 300 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "は正しい形式で入力してください。" }
-
+  validates :email, length: { maximum: 300 },
+                  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "は正しい形式で入力してください。" }
+  validates :password, length: { minimum: 6, message: "は6文字以上で入力してください" }, if: -> { password.present? }
+  
   # 検索機能（分岐）設定
   def self.search_for(content, method)
     if method == "perfect"
